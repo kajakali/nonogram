17bf3dd9413d7988442da38ee19b2349d8a2ea38
Frieda Jacobson <kajakali@hotmail.com>
2020-06-17 22:12:51 -0500

    the pattern grid component can create a visual representation of the array

--- src/components/PatternGrid/PatternGrid.js
+++ src/components/PatternGrid/PatternGrid.js
@@ -5,13 +5,21 @@ const PatternGrid =(props) => {
     const pink = '#F7BCAF';
     const orange = '#F7DEAF'; 
     const green = '#CCF7AF';
-    const array = [[0, 1, 0, 0], [1, 0, 0, 0], [0, 1, 1, 1], [1, 1, 1, 1]];
+    const array = [[pink, orange, green, green], [pink, green, orange, orange], [orange, pink, pink, green], [green, orange, pink, pink]];
         return(
             <>
                 <h1>I'm a grid!</h1>
                 <Square  backgroundColor={pink} />
                 <Square  backgroundColor={orange} />
                 <Square  backgroundColor={green} />
+                {array.map(element => (
+                    <h3>
+                        {element.map(square => (
+                            <Square backgroundColor={square} />
+                        ))}
+                    </h3>
+                    
+                ))}
 
             </>
         );
