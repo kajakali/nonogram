9bd4f831d38edeb600f45bebd16a0b960fa46b41
Frieda Jacobson <kajakali@hotmail.com>
2020-07-09 23:04:53 -0500

    array size changing buttons made small

--- src/components/ChooseArraySize/ChooseArraySize.js
+++ src/components/ChooseArraySize/ChooseArraySize.js
@@ -4,12 +4,11 @@ import Button from '@material-ui/core/Button';
 function ChooseArraySize(props) {
     return(
         <p>
-            Here we will choose the size of our pattern
-            {JSON.stringify(props.patternArray)}
-            <Button onClick={() => props.changeArraySize('wider')}>wider</Button>
-            <Button onClick={() => props.changeArraySize('narrower')}>narrower</Button>
-            <Button onClick={() => props.changeArraySize('taller')}>taller</Button>
-            <Button onClick={() => props.changeArraySize('shorter')}>shorter</Button>
+            Here we will choose the size of our pattern:
+            <Button size="small" variant="contained" color="primary" onClick={() => props.changeArraySize('wider')}>wider</Button>
+            <Button size="small" variant="contained" color="primary" onClick={() => props.changeArraySize('narrower')}>narrower</Button>
+            <Button size="small" variant="contained" color="primary" onClick={() => props.changeArraySize('taller')}>taller</Button>
+            <Button size="small" variant="contained" color="primary" onClick={() => props.changeArraySize('shorter')}>shorter</Button>
         </p>
     )
 }
