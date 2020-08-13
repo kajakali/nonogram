1ceaf2c560a2ca724f75dd0c63ccd7d90ef889cf
Frieda Jacobson <kajakali@hotmail.com>
2020-08-12 21:31:20 -0500

    split into nonogram and cross stitch pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,32 +16,83 @@ import {
 //get a table of all the patterns in the database
 function App() {
   return (
-    <div className="App">
-    <header>
+    <Router>
+        <header>
       <h1>colorful logic problems!</h1>
       <p>Hello. This is not a complete app, it's just a place for
       me to work on how to build a nonogram/griddler type puzzle in react
       Right now, you can select a color from one of the color choice squares
       and then any square you click will turn that color. I started this around June 15th, 2020.</p>
     </header>
-{/*       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div>
+      {/* <Nav /> */}
+      <Switch>
+        {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+        <Redirect exact from="/" to="/nonogram" />
+        {/* Visiting localhost:3000/about will show the about page.
+        This is a route anyone can see, no login necessary */}
+        <Route
+          exact
+          path="/nonogram"
+          component={Nonogram}
+        />
+        <Route
+          exact
+          path="/crossstitch"
+          component={CrossStitchPattern}
+        />
 
-      <Nonogram />
-      <CrossStitchPattern />
+ 
+   
+        {/* For protected routes, the view could show one of several things on the same route.
+        Visiting localhost:3000/home will show the UserPage if the user is logged in.
+        If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
+        Even though it seems like they are different pages, the user is always on localhost:3000/home */}
+  {/*       <ProtectedRoute
+          exact
+          path="/home"
+          component={UserPage}
+        /> */}
+        {/* This works the same as the other protected route, except that if the user is logged in,
+        they will see the info page instead. */}
+        {/* <ProtectedRoute
+          exact
+          path="/info"
+          component={InfoPage}
+        />
+        <ProtectedRoute
+          exact
+          path="/view/:color_id/:project_id"
+          component={ViewColor}
+        />
+        <ProtectedRoute
+          exact
+          path="/projectslist"
+          component={ProjectsList}
+        />
+        <ProtectedRoute
+          exact
+          path="/threadlist"
+          component={ThreadList}
+        />
+        <ProtectedRoute
+          exact
+          path="/addproject"
+          component={AddProject}
+        />
+
+        <ProtectedRoute
+          exact
+          path="/project/:id"
+          component={IndividualProject}
+        /> */}
+        {/* If none of the other routes matched, we will show a 404. */}
+        <Route render={() => <h1>404</h1>} />
+      </Switch>
+ {/*      <Footer /> */}
     </div>
+  </Router>
+
   );
 }
 
