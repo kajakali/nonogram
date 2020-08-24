e2d5f2af4980960ae64e76eeb30d9eeceb22e551
Frieda Jacobson <kajakali@hotmail.com>
2020-08-24 13:59:39 -0500

    added radio button selection

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -44,7 +44,6 @@ function App(props) {
               path="/crossstitch"
               component={CrossStitchPattern}
             />
-
     
       
             {/* For protected routes, the view could show one of several things on the same route.
--- src/components/SearchFunction/SearchFunction.js
+++ src/components/SearchFunction/SearchFunction.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 
 
 //this component takes an array of colors and displays it
 function SearchFunction(props) {
+  const [value, setValue] = React.useState("2");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
     <div>
     <h1>Search Function</h1>
+    <TextField>
+
+    </TextField>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Gender</FormLabel>
+      <RadioGroup aria-label="colors" name="number of colors" value={value} onChange={handleChange}>
+      {["1", "2", "3", "4", "5", "6", "7", "8"].map(number => <FormControlLabel value={number} control={<Radio />} label={number} />)}
+      </RadioGroup>
+    </FormControl>
+    <Button>
+      Search
+    </Button>
         <p> soon I'll have search options and a button to start the search</p>
     </div>
   );
